822bb60f642e43227b357d404d5c1ea7d5b812d4
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2022-10-20 22:03:16 -0300

    add produto controller

--- E-commerce/src/main/java/org/serratec/ecommerce/api/repository/PedidoRepository.java
+++ E-commerce/src/main/java/org/serratec/ecommerce/api/repository/PedidoRepository.java
@@ -2,7 +2,8 @@ package org.serratec.ecommerce.api.repository;
 
 import org.serratec.ecommerce.api.domain.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface PedidoRepository extends JpaRepository<Produto, Long>{
 
 }
