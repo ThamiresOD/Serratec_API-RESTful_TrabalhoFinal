31d46f34a36a9c15ff53ead71044db15d8a3f189
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2022-10-20 21:33:06 -0300

    Add repositories

---
+++ E-commerce/src/main/java/org/serratec/ecommerce/api/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package org.serratec.ecommerce.api.repository;
+
+import org.serratec.ecommerce.api.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository<Produto, Long>{
+
+}
+
