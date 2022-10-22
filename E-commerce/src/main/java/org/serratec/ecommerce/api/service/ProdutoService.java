601bcbe95ff6af8ded50dfd8d17e14f0fa7d92ab
RivaniaGomes <rivaniagomes@gmail.com>
2022-10-22 16:44:59 -0300

    Inclusão de GET e GET{id} com DTO.

--- E-commerce/src/main/java/org/serratec/ecommerce/api/service/ProdutoService.java
+++ E-commerce/src/main/java/org/serratec/ecommerce/api/service/ProdutoService.java
@@ -1,6 +1,7 @@
 package org.serratec.ecommerce.api.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.serratec.ecommerce.api.domain.Produto;
 import org.serratec.ecommerce.api.repository.ProdutoRepository;
@@ -10,15 +11,23 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProdutoService {
 
-	@Autowired 
+	@Autowired
 	private ProdutoRepository prodRepo;
-	
-	public List<Produto> findAll(){
+
+	public List<Produto> findAll() {
 		return prodRepo.findAll();
 	}
-	
+
 	public Produto inserir(Produto produto) {
 		return prodRepo.save(produto);
 	}
-	
+
+	public Optional<Produto> findById(Long id) {
+		return prodRepo.findById(id);
+	}
+
+	public void deleteById(Long id) {
+		prodRepo.deleteById(id);
+	}
+
 }
