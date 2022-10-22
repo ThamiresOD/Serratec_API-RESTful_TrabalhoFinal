a7528632a5c4a5c9302b3f16c983f551af82a5fa
Breno Leonardo ferreira de Paula <breno.leonardof@hotmail.com>
2022-10-22 18:57:01 -0300

    Create ControllerExceptionHandler.java

---
+++ E-commerce/src/main/java/org/serratec/ecommerce/api/exception/ControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package org.serratec.ecommerce.api.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	      @ExceptionHandler(EmailException.class)
+	      public ResponseEntity<Object> emailExceptionHandler(EmailException ex){
+		            return ResponseEntity.unprocessableEntity().body(ex.getMessage());
+		
+	      }
+}
