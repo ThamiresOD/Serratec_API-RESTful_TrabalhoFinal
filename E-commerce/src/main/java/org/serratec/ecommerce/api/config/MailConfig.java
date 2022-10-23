214e93725aaabad8e9d4d9cc89e606de93e08ae5
Leonardo Ammon <leonardo.a.ammon@gmail.com>
2022-10-22 21:33:12 -0300

    tira email exception

--- E-commerce/src/main/java/org/serratec/ecommerce/api/config/MailConfig.java
+++ E-commerce/src/main/java/org/serratec/ecommerce/api/config/MailConfig.java
@@ -1,23 +1,23 @@
-package br.org.serratec.projeto.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
-@Configuration
-public class MailConfig {
-	      @Autowired
-	      JavaMailSender javaMailSender;
-
-	      public void sendMail(String para, String assunto, String texto) {
-		            SimpleMailMessage message = new SimpleMailMessage();
-		            message.setFrom("breno.leonardof10@gmail.com"); //pode trocar por outro email de alguém no grupo
-		            message.setSubject(assunto);
-		            message.setText(texto);
-		            message.setTo(para);
-		            javaMailSender.send(message);
-        //a validacao atraves do cadastro e realizada no service
-
-	      }
-}
+//package br.org.serratec.projeto.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//
+//@Configuration
+//public class MailConfig {
+//	      @Autowired
+//	      JavaMailSender javaMailSender;
+//
+//	      public void sendMail(String para, String assunto, String texto) {
+//		            SimpleMailMessage message = new SimpleMailMessage();
+//		            message.setFrom("breno.leonardof10@gmail.com"); //pode trocar por outro email de alguém no grupo
+//		            message.setSubject(assunto);
+//		            message.setText(texto);
+//		            message.setTo(para);
+//		            javaMailSender.send(message);
+//        //a validacao atraves do cadastro e realizada no service
+//
+//	      }
+//}
