7e088ff6bc8fc29077ca024c6d93585601f619e5
RivaniaGomes <rivaniagomes@gmail.com>
2022-10-23 17:57:13 -0300

    corrigindo a alteração indevida que fiz nos inserts da thamires

--- E-commerce/src/main/resources/db/migration/V03__insert_tabelas_cliente_produto.sql
+++ E-commerce/src/main/resources/db/migration/V03__insert_tabelas_cliente_produto.sql
@@ -12,20 +12,15 @@ INSERT INTO cliente(cli_tx_nome_completo, cli_tx_email, cli_tx_cpf, cli_tx_telef
 ;
 
 --Produto
-
-INSERT INTO produto(prd_tx_nome, prd_tx_descricao, prd_int_quantidade_estoque, prd_dt_data_cadastro, prd_nm_valor_unitario, prd_tx_url_produto, cat_cd_id) VALUES 
-	('Boba feet', 'Boneco Boba feet', 10, '2022-06-04', 110, NULL, 1),
-  ('Leia Organa', 'Boneco Leia Organa', 10, '2022-06-04', 110, NULL, 1),
- 	('Yoda', 'Boneco Yoda', 10, '2022-06-04', 110, NULL, 1),
-	('Batman', 'Boneco Batman', 10, '2022-06-04', 110, NULL, 2),
+INSERT INTO produto(prd_tx_nome, prd_tx_descricao, prd_int_quantidade_estoque, 
+	prd_dt_data_cadastro, prd_nm_valor_unitario, prd_tx_url_produto, cat_cd_id) VALUES 
 	('API', 'Caixão da Felicidade', 36, '2022-10-30', 1000.0, NULL, 2), 
-  ('Vasco', 'Caixão da serie a', 8560, '2008-01-05', 110.00, NULL, 1), 
-  ('Copinha', 'Caixão do mundial', 5230, '1950-04-15', 11000.00, NULL, 2), 
+    ('Vasco', 'Caixão da serie a', 8560, '2008-01-05', 110.00, NULL, 1), 
+    ('Copinha', 'Caixão do mundial', 5230, '1950-04-15', 11000.00, NULL, 2), 
 	('Leonardo', 'Caixão do Windows, linha Linux', 2, '2022-10-20', 10.0, NULL, 1),
 	('Rivania', 'Caixão da paz', 11, '2022-08-06', 2250.00, NULL, 3), 
 	('Fabio', 'Caixão do amor', 1, '2022-10-12', 2250.00, NULL, 4), 
 	('Cleiver', 'Caixão do backend', 200, '2022-10-29', 2250.00, NULL, 3),  
 	('Goku', 'Caixão do cabelo', 2, '2022-10-16', 2000.00, NULL, 1),  
 	('Serratec', 'Caixão da vida social', 148, '2022-08-06', 10000.00, NULL, 4)  
-;
 
