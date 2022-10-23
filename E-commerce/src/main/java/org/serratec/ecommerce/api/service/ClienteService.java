26af15433ad836765c65c2c10c40f7343a43068e
Breno Leonardo ferreira de Paula <breno.leonardof@hotmail.com>
2022-10-22 22:54:32 -0300

    Update ClienteService.java

--- E-commerce/src/main/java/org/serratec/ecommerce/api/service/ClienteService.java
+++ E-commerce/src/main/java/org/serratec/ecommerce/api/service/ClienteService.java
@@ -2,11 +2,8 @@ package org.serratec.ecommerce.api.service;
 
 import java.util.List;
 
-//import org.serratec.ecommerce.api.config.MailConfig;
 import org.serratec.ecommerce.api.domain.Cliente;
 import org.serratec.ecommerce.api.domain.dto.ClienteInserirDTO;
-//import org.serratec.ecommerce.api.exception.CpfException;
-//import org.serratec.ecommerce.api.exception.EmailException;
 import org.serratec.ecommerce.api.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,15 +15,7 @@ public class ClienteService {
 	@Autowired
 	EnderecoService enderecoService;
 	
-	public Cliente inserir(ClienteInserirDTO novoClienteDTO){ /*throws EmailException, throws CpfException{
-		Cliente clienteBanco = clienteRepository.findByEmail(novoClienteDTO.getEmail());
-		if (clienteBanco!=null) {
-			throw new EmailException("Já existe um cliente com esse mesmo e-mail registrado!" + novoClienteDTO.getEmail());
-		}
-		Cliente clienteCpf = clienteRepository.findByCpf(novoClienteDTO.getCpf());
-		if (clienteCpf!=null) {
-			throw new CpfException("Já existe um cliente com esse mesmo cpf registrado!" + novoClienteDTO.getCpf());
-		}*/
+	public Cliente inserir(ClienteInserirDTO novoClienteDTO){
 		Cliente novoClienteDB = new Cliente();
 		novoClienteDB.setNomeCompleto(novoClienteDTO.getNomeCompleto());
 		novoClienteDB.setCpf(novoClienteDTO.getCpf());
