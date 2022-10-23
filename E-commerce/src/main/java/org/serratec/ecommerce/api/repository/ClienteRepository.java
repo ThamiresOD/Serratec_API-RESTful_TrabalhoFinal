a05b5f311b9d8ff4ac77b743816c9a4b31bf1471
Breno Leonardo ferreira de Paula <breno.leonardof@hotmail.com>
2022-10-22 21:52:16 -0300

    Update ClienteRepository.java

--- E-commerce/src/main/java/org/serratec/ecommerce/api/repository/ClienteRepository.java
+++ E-commerce/src/main/java/org/serratec/ecommerce/api/repository/ClienteRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
-
+        Cliente findByEmail(String email);
+        Cliente findByCpf(String cpf);
 }
 
